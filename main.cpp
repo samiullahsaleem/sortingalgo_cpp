7e05eb06b92e5cdb21f9c714102dd2508e13cc1d
Sami Ullah Saleem <54318487+samiullahsaleem@users.noreply.github.com>
2022-08-14 14:22:54 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int n ;
+    cin >>n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    // Selection Sort
+    // for(int i = 0; i < n-1; i++)
+    // {
+    //     for(int j = i+1; j < n; j++)
+    //     {
+    //         if(arr[j] < arr[i])
+    //         {
+    //             int temp = arr[j];
+    //             arr[j] = arr[i];
+    //             arr[i] = temp;
+    //         }
+    //     }
+    // }
+    
+    // Bubble Sort
+    
+    int counter = 1;
+    while(counter < n)
+    {
+        for(int i =0; i < n -counter; i++)
+        {
+            if(arr[i] > arr[i+1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        counter++;
+    }
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+}
